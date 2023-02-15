e0eb714cd8253fffdf990665d92ff858a8238ff0
fuchiue <2220235@ecc.ac.jp>
2023-02-15 14:55:06 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tabenikki/CreateMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.tabenikki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_menu);
+    }
+}
