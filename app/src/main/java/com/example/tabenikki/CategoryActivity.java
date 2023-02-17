39046ccd6f01e781327ed1c021d01f2479ac04df
fuchiue <2220235@ecc.ac.jp>
2023-02-17 14:00:08 +0900

    Categoryでデータの保存確率

--- app/src/main/java/com/example/tabenikki/CategoryActivity.java
+++ app/src/main/java/com/example/tabenikki/CategoryActivity.java
@@ -2,7 +2,13 @@ package com.example.tabenikki;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CategoryActivity extends AppCompatActivity {
 
@@ -10,5 +16,32 @@ public class CategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+        //SharedPreferencesの生成
+        SharedPreferences data = getSharedPreferences("Data", MODE_PRIVATE);
+        //入力用editor生成
+        SharedPreferences.Editor editor = data.edit();
+
+        //arraylist作成
+        ArrayList<String> strlist = new ArrayList<>();
+        strlist.add("カフェ");
+        strlist.add("ラーメン");
+        //リファレンスに保存する名前を作成
+        String list = "カテゴリ";
+        //arrayListをString変換してリファレンスに登録
+        editor.putString(list,String.join(",",strlist));
+        //データの確定COMMIT
+        editor.apply();
+
+        //リファレンスからデータを取得
+        String onString = data.getString(list,"");
+        System.out.println(onString);
+        //変換してコンマ区切りになったStringデータをアレイリストに変換
+        ArrayList<String> splitStr = new ArrayList<>(Arrays.asList(onString.split("\\s*,\\s*")));
+        System.out.println(splitStr);
+
+
+
+
+
     }
 }
